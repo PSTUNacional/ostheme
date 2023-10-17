64127d36f881ddcf543a4012df53279e1ecb2633
J. H. Mendoza <jhmendoza@iMac-de-Jorge.local>
2023-10-17 18:17:42 -0300

    Panel fix

--- components/header_block_01.php
+++ components/header_block_01.php
@@ -50,7 +50,7 @@
             <div class="divider"></div>
             <div class="col col-25">
                 <?php
-                $args = array('numberposts' => 1, 'category' => 19);
+                $args = array('numberposts' => 1, 'category_name' => 'editorial',);
                 $editorial = get_posts($args);
                 ?>
                 <article class="article-editorial">
