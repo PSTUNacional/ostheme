e8fd2d65bd4d7f7db69353ae2fef247b724c760e
J. H. Mendoza <jhmendoza@iMac-de-Jorge.local>
2023-10-20 18:58:21 -0300

    video section

--- components/block_video_01.php
+++ components/block_video_01.php
@@ -3,7 +3,7 @@ $url = 'https://data.pstu.org.br/src/Api/YoutubeContent.php?method=listchannels'
 $channels = file_get_contents($url);
 $channels = json_decode($channels, true);
 ?>
-<section class="video-section">
+<section class="video-section" style="background-color:var(--grey-700);">
     <div class="container">
         <div class="col">
             <h2 class="block-header">
@@ -18,14 +18,14 @@ $channels = json_decode($channels, true);
                     $videos = file_get_contents($url);
                     $videos = json_decode($videos, true);
                 ?>
-                    <div class="video-item" data-video="<?= $videos[$i]['video_id'] ?>">
-                        <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[$i]['video_id'] ?>/mqdefault.jpg')"></div>
+                    <div class="video-item" data-video="<?= $videos[0]['video_id'] ?>">
+                        <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[0]['video_id'] ?>/mqdefault.jpg')"></div>
                         <div class="video-info">
-                            <span class="badge primary"><?= $videos[$i]['channel_name'] ?></span>
-                            <h3><?= $videos[$i]['title'] ?></h3>
-                            <span class="date"><?= date("d M Y", strtotime($videos[$i]['date'])) ?></span>
-                            <p class="description"><?= $videos[$i]['description'] ?></p>
-                            <p class="duration"><?= $videos[$i]['duration'] ?></p>
+                            <span class="badge primary"><?= $videos[0]['channel_name'] ?></span>
+                            <h3><?= $videos[0]['title'] ?></h3>
+                            <span class="date"><?= date("d M Y", strtotime($videos[0]['date'])) ?></span>
+                            <p class="description"><?= $videos[0]['description'] ?></p>
+                            <p class="duration"><?= $videos[0]['duration'] ?></p>
                         </div>
                     </div>
                 <?php } ?>
