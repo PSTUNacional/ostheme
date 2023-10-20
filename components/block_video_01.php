df02dac743429508d7e90a5fe76f8edde37873a9
J. H. Mendoza <jhmendoza@iMac-de-Jorge.local>
2023-10-20 18:56:16 -0300

    video section

--- components/block_video_01.php
+++ components/block_video_01.php
@@ -10,34 +10,26 @@ $channels = json_decode($channels, true);
                 <span><?= get_cat_name($cat) ?></span>
                 <a href="<?= get_category_link($cat) ?>" class="more">Ver todos</a>
             </h2>
-            <?php
-        foreach ($channels as $channel) {
-            $url = 'https://data.pstu.org.br/src/Api/YoutubeContent.php?method=listvideos&channel=' . $channel['id'];
-            $videos = file_get_contents($url);
-            $videos = json_decode($videos, true);
-        ?>
-            <div class="channel-section">
-                <h3 class="channel-title"><?= $channel['name'] ?></h3>
-                <div class="video-container">
-                    <?php
-                    for ($i = 0; $i < 10; $i++) {
-                    ?>
 
-                        <div class="video-item" data-video="<?= $videos[$i]['video_id'] ?>">
-                            <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[$i]['video_id'] ?>/mqdefault.jpg')"></div>
-                            <div class="video-info">
-                                <span class="badge primary"><?= $videos[$i]['channel_name'] ?></span>
-                                <h3><?= $videos[$i]['title'] ?></h3>
-                                <span class="date"><?= date("d M Y", strtotime($videos[$i]['date'])) ?></span>
-                                <p class="description"><?= $videos[$i]['description'] ?></p>
-                                <p class="duration"><?= $videos[$i]['duration'] ?></p>
-                            </div>
+            <div class="video-container">
+                <?php
+                foreach ($channels as $channel) {
+                    $url = 'https://data.pstu.org.br/src/Api/YoutubeContent.php?method=listvideos&channel=' . $channel['id'];
+                    $videos = file_get_contents($url);
+                    $videos = json_decode($videos, true);
+                ?>
+                    <div class="video-item" data-video="<?= $videos[$i]['video_id'] ?>">
+                        <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[$i]['video_id'] ?>/mqdefault.jpg')"></div>
+                        <div class="video-info">
+                            <span class="badge primary"><?= $videos[$i]['channel_name'] ?></span>
+                            <h3><?= $videos[$i]['title'] ?></h3>
+                            <span class="date"><?= date("d M Y", strtotime($videos[$i]['date'])) ?></span>
+                            <p class="description"><?= $videos[$i]['description'] ?></p>
+                            <p class="duration"><?= $videos[$i]['duration'] ?></p>
                         </div>
-
-                    <?php } ?>
-                </div>
+                    </div>
+                <?php } ?>
             </div>
-        <?php } ?>
         </div>
     </div>
 </section>
