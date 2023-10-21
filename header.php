8876202789aa74186f8ae614403f4e4028aa9aac
J. H. Mendoza <jhmendoza@iMac-de-Jorge.local>
2023-10-21 10:33:04 -0300

    meta test

--- header.php
+++ header.php
@@ -8,10 +8,7 @@
 	<meta http-equiv="content-language" content="pt-br">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="robots" content="follow">
-	<link rel="canonical" href="https://www.opiniaosocialista.com.br"/>
-	<meta name="description" content="Author: A.N. Author,
-    Illustrator: P. Picture, Category: Books, Price: $17.99,
-    Length: 784 pages">
+	<meta name="description" content="O jornal oficial do PSTU. Uma visão socialista do mundo a serviço da classa trabalhadora.">
 
 	<meta property="og:title" content="<?php is_front_page() ? bloginfo('name') : wp_title('|', 1, 'right').' | '.bloginfo('name'); ?>"/>
 	<meta property="og:type" content="article"/>
