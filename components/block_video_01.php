2b62eb97f6cfd3883fb365eba88787e73eaa416d
J. H. Mendoza <jhmendoza@iMac-de-Jorge.local>
2023-10-21 13:17:48 -0300

    Video block

--- components/block_video_01.php
+++ components/block_video_01.php
@@ -13,12 +13,21 @@ $channels = json_decode($channels, true);
 
             <div class="video-container">
                 <?php
-                foreach ($channels as $channel) {
+                $videos_arr = [];
+                foreach ($channels as $channel)
+                {
                     $url = 'https://data.pstu.org.br/src/Api/YoutubeContent.php?method=listvideos&channel=' . $channel['id'];
                     $videos = file_get_contents($url);
                     $videos = json_decode($videos, true);
+
+                    array_push($videos_arr, $videos[0]);
+                } 
+
+                var_dump($videos_arr);
                 ?>
-                    <div class="video-item" data-video="<?= $videos[0]['video_id'] ?>">
+
+<!--
+                <div class="video-item" data-video="<?= $videos[0]['video_id'] ?>">
                         <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos[0]['video_id'] ?>/mqdefault.jpg')"></div>
                         <div class="video-info">
                             <span class="badge primary"><?= $videos[0]['channel_name'] ?></span>
@@ -28,7 +37,10 @@ $channels = json_decode($channels, true);
                             <p class="duration"><?= $videos[0]['duration'] ?></p>
                         </div>
                     </div>
-                <?php } ?>
+
+
+            -->
+
             </div>
         </div>
     </div>
