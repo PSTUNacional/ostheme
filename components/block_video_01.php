2299a9f55d06aec069b10fa2ebff2c3b59da5617
J. H. Mendoza <jhmendoza@iMac-de-Jorge.local>
2023-10-22 13:16:30 -0300

    Featured Image lazy load

--- components/block_video_01.php
+++ components/block_video_01.php
@@ -41,28 +41,32 @@ $channels = json_decode($channels, true);
                     </a>
                 </div>
                 <div class="col">
-                    <a target="_blank" href="https://www.youtube.com/watch?v=<?= $videos_arr[0]['video_id'] ?>" aria-label="Clique para assistir o vídeo" class="video-item" data-video="<?= $videos_arr[1]['video_id'] ?>">
-                        <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos_arr[1]['video_id'] ?>/mqdefault.jpg')"></div>
+                    <div target="_blank" href="https://www.youtube.com/watch?v=<?= $videos_arr[1]['video_id'] ?>" aria-label="Clique para assistir o vídeo" class="video-item" data-video="<?= $videos_arr[1]['video_id'] ?>">
+                        <a target="_blank" href="https://www.youtube.com/watch?v=<?= $videos_arr[2]['video_id'] ?>" aria-label="Clique para assistir o vídeo" class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos_arr[1]['video_id'] ?>/mqdefault.jpg')"></a>
                         <div class="video-info">
                             <span class="badge primary"><?= $videos_arr[1]['channel_name'] ?></span>
-                            <h3><?= $videos_arr[1]['title'] ?></h3>
+                            <a target="_blank" href="https://www.youtube.com/watch?v=<?= $videos_arr[1]['video_id'] ?>" aria-label="Clique para assistir o vídeo">
+                                <h3><?= $videos_arr[1]['title'] ?></h3>
+                            </a>
                             <span class="date"><?= date("d M Y", strtotime($videos_arr[1]['date'])) ?></span>
                             <p class="description"><?= $videos_arr[1]['description'] ?></p>
                             <p class="duration"><?= $videos_arr[1]['duration'] ?></p>
                         </div>
-                    </a>
-                    <a target="_blank" href="https://www.youtube.com/watch?v=<?= $videos_arr[0]['video_id'] ?>" aria-label="Clique para assistir o vídeo" class="video-item" data-video="<?= $videos_arr[2]['video_id'] ?>">
-                        <div class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos_arr[2]['video_id'] ?>/mqdefault.jpg')"></div>
+                    </div>
+                    <div class="video-item" data-video="<?= $videos_arr[2]['video_id'] ?>">
+                        <a target="_blank" href="https://www.youtube.com/watch?v=<?= $videos_arr[2]['video_id'] ?>" aria-label="Clique para assistir o vídeo" class="video-thumb" style="background-image:url('https://i.ytimg.com/vi/<?= $videos_arr[2]['video_id'] ?>/mqdefault.jpg')"></a>
                         <div class="video-info">
                             <span class="badge primary"><?= $videos_arr[2]['channel_name'] ?></span>
-                            <h3><?= $videos_arr[2]['title'] ?></h3>
+                            <a target="_blank" href="https://www.youtube.com/watch?v=<?= $videos_arr[2]['video_id'] ?>" aria-label="Clique para assistir o vídeo">
+                                <h3><?= $videos_arr[2]['title'] ?></h3>
+                            </a>
                             <span class="date"><?= date("d M Y", strtotime($videos_arr[2]['date'])) ?></span>
                             <p class="description"><?= $videos_arr[2]['description'] ?></p>
                             <p class="duration"><?= $videos_arr[2]['duration'] ?></p>
                         </div>
-                    </a>
+                        </dibv>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 </section>
