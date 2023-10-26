3a5168c3f5347fda0c132321245c58b36dfda55a
J. H. Mendoza <jhmendoza@iMac-de-Jorge.local>
2023-10-26 19:06:34 -0300

    Banners

--- banner.php
+++ banner.php
@@ -36,18 +36,44 @@ get_header(); ?>
             width: 100%;
             height: 100%;
             max-width: 180px;
+            background-size:cover;
+            background-position:Center;
+            cursor:pointer;
+        }
+
+        .banner-grid .info{
+            display: none;
+            transition: all .3s ease-in-out;
+            gap:8px;
+            width: 100%;
+            height: 100%;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .banner-grid .info a{
+            width: 32px;
+            height:32px;
+            border-radius:50%;
+            background-color: var(--primary);
+        }
+
+        .banner-grid .banner-card:hover .info{
+            display: flex;
         }
 
     </style>
     <main>
+        <div class="container">
         <div class="banner-grid">
             <?php
 
             foreach($banners as $b)
             {
-                echo '<div class="banner-card" style="background-image:url(\'https://opiniaosocialista.com.br/automation/assets/rendered/'.$b.'\')"></div>';
+                echo '<div class="banner-card" style="background-image:url(\'https://opiniaosocialista.com.br/automation/assets/rendered/'.$b.'\')"><div class="info"><a></a><a></a></div></div>';
             }
             ?>
+        </div>
         </div>
     </main>
 </div>
