874d667f0f64baec5a11c5a7f1784b4b1d99acd9
J. H. Mendoza <jhmendoza@iMac-de-Jorge.local>
2023-10-26 18:54:19 -0300

    Banners

--- banner.php
+++ banner.php
@@ -7,13 +7,29 @@ Template Name: Banners
 $dir = $_SERVER['DOCUMENT_ROOT'].'/automation/assets/rendered/';
 $bannerList = scandir($dir);
 
+$banners = [];
+$stories = [];
+
+foreach($bannerList as $content)
+{
+    if(strpos($content, 'OS-B') == true )
+    {
+        array_push($banners, $content);
+    }
+    if(strpos($content, 'OS-S') == true )
+    {
+        array_push($stories, $content);
+    }
+}
+
 
 get_header(); ?>
 
 <div class="content-area">
     <main>
         <pre>
-            <?=print_r($bannerList)?>
+            <?=print_r($banners)?>
+            <?=print_r($stories)?>
         </pre>
     </main>
 </div>
