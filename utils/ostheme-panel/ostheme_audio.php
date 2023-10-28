29e450df6a3f925652e1d7c0a73340eb00d69853
J. H. Mendoza <jhmendoza@iMac-de-Jorge.local>
2023-10-28 18:19:26 -0300

    Audio section

--- utils/ostheme-panel/ostheme_audio.php
+++ utils/ostheme-panel/ostheme_audio.php
@@ -9,7 +9,8 @@ function render_theme_audio()
 {
     $options = $GLOBALS['options'];
 ?>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="../wp-content/themes/ostheme/assets/css/buttons.css">
+    <link rel="stylesheet" href="../wp-content/themes/ostheme/assets/css/colors.css">
     <style>
         form {
             display: flex;
@@ -27,6 +28,48 @@ function render_theme_audio()
             display: flex;
             gap: 4px;
             flex-direction: column;
+        }
+
+        .admin-table .dashicons {
+            font-size: 16px;
+            margin: 0 6px;
+            color: #999;
+            line-height: 1.25rem;
+        }
+
+        .search {
+            position: relative;
+        }
+
+        input:focus+.search-result,
+        .search .search-result:hover {
+            display: flex;
+        }
+
+        .search .search-result {
+            display: none;
+            flex-direction: column;
+            gap: 4px;
+            position: absolute;
+            border: 1px solid #999;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 3px 6px 0 #0006;
+            bottom: 0;
+            transform: translateY(100%);
+            width: 100%;
+            box-sizing: border-box;
+            overflow-y: scroll;
+            max-height: 320px;
+        }
+
+        .result {
+            cursor: pointer;
+            padding: 4px 12px
+        }
+
+        .result:hover {
+            background-color: #fee;
         }
     </style>
     <h1>Ombudsman</h1>
@@ -46,12 +89,16 @@ function render_theme_audio()
         </table>
         <div class="card" style="margin-top:0">
             <h3 id="post-title">Adicionar um áudio</h3>
-            <form>
-                <div class="line">
+            <form id="audioform">
+                <div class="line search">
                     <label for="post">Matéria</label>
-                    <input type="text" name="post" id="" />
+                    <input type="text" name="title" id="" />
+                    <div class="search-result">
+                        <p>Procurando resultados...</p>
+                    </div>
                 </div>
-                <input type="file" name="file" id="" accept=".mp3,.wav,.flac,.ogg">
+
+                <input type="file" name="audiofile" id="" accept=".mp3,.wav,.flac,.ogg">
                 <div class="line">
                     <label for="authorName">Locutor</label>
                     <input type="text" name="authorName" id="" />
@@ -59,16 +106,44 @@ function render_theme_audio()
                 <div class="line">
                     <label for="tags">Tags</label>
                     <input type="text" name="tags" id="" />
+                    <p>Identifique o coteúdo separando as tags por vírgula. Por exemplo: <b>Editorial, OS633</b>.
                 </div>
+                <input style="display:none" type="text" name="id" id="" />
+                <input style="display:none" type="text" name="url" id="" />
                 <div class="line">
-                    <input class="btn primary" type="submit" value="Postar" />
+                    <button onclick="validate()" class="btn primary" value="Postar">Enviar</button>
                 </div>
             </form>
         </div>
     </div>
 
     <script>
-        fetch('https://www.opiniaosocialista.com.br/automation/src/Controller/Content.php?method=getByType&type=audio&offset=30')
+        document.querySelector('.search input').addEventListener('input', (event) => {
+            p = document.querySelector('.search-result')
+
+            s = event.target.value.toLowerCase()
+            s.toLowerCase()
+            if (s.length > 3) {
+                p.innerHTML = '';
+                fetch('https://www.opiniaosocialista.com.br/wp-json/wp/v2/search?_embed&search=' + s)
+                    .then(resp => resp.json())
+                    .then(results => {
+                        results.forEach((r) => {
+                            item = '<div class="result" onclick="setPost(' + r['id'] + ',\'' + r['url'] + '\')">' + r['title'] + '</div>'
+                            p.innerHTML += item
+                        })
+                    })
+            }
+        })
+
+        function setPost(id, url) {
+            form = document.getElementById('audioform');
+            form.title.value = event.target.innerText
+            form.id.value = id
+            form.url.value = url
+        }
+
+        fetch('https://www.opiniaosocialista.com.br/automation/src/Controller/Content.php?method=getByType&type=audio&limit=30')
             .then(resp => resp.json())
             .then(data => {
                 data.forEach(result => {
@@ -78,54 +153,36 @@ function render_theme_audio()
                 })
             })
 
-        async function getComments(id) {
-            await fetch('/wp-content/themes/ostheme/src/Controller/Evaluations.php?method=getAllComments&id=' + id)
-                .then(resp => resp.json())
-                .then(data => {
-                    document.getElementById('post-title').innerText = data[0]['title']
-                    document.getElementById('comments').innerHTML = ''
 
-                    ratefive = 0
-                    ratefour = 0
-                    ratethree = 0
-                    ratetwo = 0
-                    rateone = 0
 
-                    data.forEach(comment => {
-                        switch (comment['rate']) {
-                            case '5':
-                                ratefive++
-                                break;
-                            case '4':
-                                ratefour++
-                                break;
-                            case '3':
-                                ratethree++
-                                break;
-                            case '2':
-                                ratetwo++
-                                break;
-                            case '1':
-                                rateone++
-                                break;
-                        }
-                        if (comment['comment'] !== '') {
-                            let stars = ''
-                            for (i = 0; i < comment['rate']; i++) {
-                                stars += '<span class="dashicons dashicons-star-filled"></span>'
-                            }
+        function validate() {
+            event.preventDefault()
+            form = document.getElementById('audioform');
+            if (form.title.value == '') {
+                window.alert('É preciso inserir uma matéria.')
+                return false
+            }
+            if (form.audiofile.files.length !== 1) {
+                window.alert('É preciso inserir um arquivo.')
+                return false
+            }
+            if (form.authorName.value == '') {
+                window.alert('É preciso inserir um autor.')
+                return false
+            }
 
-                            tr = '<div class="comment-card"><div class="stars">' + stars + '</div>' + comment['comment'] + '<div>'
-                            document.getElementById('comments').innerHTML += tr
-                        }
+            data = new FormData();
 
-                        document.getElementById('ratefive').innerText = ratefive
-                        document.getElementById('ratefour').innerText = ratefour
-                        document.getElementById('ratethree').innerText = ratethree
-                        document.getElementById('ratetwo').innerText = ratetwo
-                        document.getElementById('rateone').innerText = rateone
-                    })
-                })
+            data.append('id', form.id.value)
+            data.append('url', form.url.value)
+            data.append('title', form.title.value)
+            data.append('tags', form.tags.value)
+            data.append('file', form.audiofile.files[0])
+
+            fetch("https://www.opiniaosocialista.com.br/automation/src/Controller/Content.php?method=createAudio", {
+                method: "POST",
+                body: form,
+            })
         }
     </script>
 <?php
