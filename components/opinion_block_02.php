c95d4031ed8aeaaa715b6ec479be65552d0dcb36
J. H. Mendoza <romerito.pontes@monitoratec.com.br>
2025-05-20 14:47:59 -0300

    Fix Opinion Block 02 to show only one text by author

--- components/opinion_block_02.php
+++ components/opinion_block_02.php
@@ -1,14 +1,40 @@
 <?php
 
 $args = [
-	"cat" => 3793,
-	'posts_per_page' => 10,
+	"category_name" => "colunas",
+	'posts_per_page' => 50,
     'order' => 'DESC',
     'orderby' => 'date'
 ];
-
 $posts = new WP_Query($args);
 
+// Author Filter
+
+$authors = [];
+$columns = [];
+
+if($posts->have_posts())
+{
+	while($posts->have_posts())
+	{
+		$posts->the_post();
+		$authorId = get_the_author_meta('ID');
+		
+		if(!in_array($authorId, $authors))
+		{
+			$authors[] = $authorId;
+			$columns[] = [
+				'title' => get_the_title(),		
+				'permalink' => get_permalink(),
+				'author_name' => get_the_author_meta('display_name'),
+				'author_slug' => get_the_author_meta('user_nicename'),
+				'author_avatar' => get_avatar_url($authorId)
+			];
+		}
+		if (count($columns) >= 10) break;
+	}
+}
+wp_reset_postdata();
 ?>
 
 <section>
@@ -19,26 +45,23 @@ $posts = new WP_Query($args);
                 <a href="categoria/colunas" class="more">Ver todos</a>
             </h2>
             <div class="opinion-block-01">
-                <?php if ( $posts->have_posts() ) {  
-					while ( $posts->have_posts() ) {
-						$posts->the_post();
-						$profile = get_avatar_url( get_the_author_meta('ID'));
-                    ?>
+                <?php foreach ($columns as $column): ?>
                     <article>
                         <div class="author-box">
-                            <div class="author-avatar" style="background-image:url('<?=$profile?>')">
-                            <?php
-                                if(!$profile){ ?>
+                            <div class="author-avatar" style="background-image:url('<?= esc_url($column['author_avatar']) ?>')">
+                                <?php if (empty($column['author_avatar'])): ?>
                                     <i class="fa fa-user"></i>
-                                <?php } ?>
+                                <?php endif; ?>
                             </div>
-                            <span class="author-line"><a href="coluna/<?=the_author_meta('user_nicename')?>"><?= the_author_meta('display_name'); ?></a></span>
+                            <span class="author-line">
+                                <a href="coluna/<?= esc_attr($column['author_slug']) ?>"><?= esc_html($column['author_name']) ?></a>
+                            </span>
                         </div>
-                        <a href="<?= the_permalink() ?>" title="<?= the_title() ?>">
-                            <h2><?= the_title(); ?></h2>
+                        <a href="<?= esc_url($column['permalink']) ?>" title="<?= esc_attr($column['title']) ?>">
+                            <h2><?= esc_html($column['title']) ?></h2>
                         </a>
                     </article>
-                <?php  } }   ?>
+                <?php endforeach; ?>
             </div>
         </div> 
     </div>
